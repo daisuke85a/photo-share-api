4e87da1a3c00d5c030bcfe9ae16e18b8f271a8b9
daisuke <dsaito85a@gmail.com>
2020-12-25 18:35:34 +0900

    多対多のコネクション

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const typeDefs= `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   type User {
@@ -15,6 +16,7 @@ const typeDefs= `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   input PostPhotoInput {
@@ -72,6 +74,13 @@ const photos = [
   },
 ]
 
+const tags = [
+  { "photoID": "1", "userID": "gPlake" },
+  { "photoID": "2", "userID": "sSchmidt" },
+  { "photoID": "2", "userID": "mHattrup" },
+  { "photoID": "2", "userID": "gPlake" },
+]
+
 
 const resolvers = {
   Query: {
@@ -93,11 +102,19 @@ const resolvers = {
     postedBy: parent => {
       return users.find(u => u.githubLogin == parent.githubUser)
     },
+    taggedUsers: parent => tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
-    }
+    },
+    inPhotos: parent => tags
+        .filter(tag => tag.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
