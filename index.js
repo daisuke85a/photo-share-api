4b12621ce4951a91ddc3b8b57be34a205f95913e
daisuke <dsaito85a@gmail.com>
2020-12-25 18:02:02 +0900

    add: １対多の接続

--- index.js
+++ index.js
@@ -1,63 +1,109 @@
 const { ApolloServer } = require(`apollo-server`)
 
 const typeDefs= `
-    type Photo {
-        id: ID!
-        url: String!
-        name: String!
-        description: String
-        category: PhotoCategory!
-    }
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+    category: PhotoCategory!
+    postedBy: User!
+  }
 
-    input PostPhotoInput {
-        name: String!
-        category: PhotoCategory=PORTRAIT
-        description: String
-    }
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
 
-    type Query {
-        totalPhotos: Int!
-        allPhotos: [Photo!]!
-    }
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory=PORTRAIT
+    description: String
+  }
 
-    type Mutation {
-        postPhoto(input: PostPhotoInput!): Photo!
-    }
+  type Query {
+    totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
 
-    enum PhotoCategory {
-        SELFIE
-        PORTRAIT
-        ACTION
-        LANDSCAPE
-        GRAPHIC
-    }
+  type Mutation {
+    postPhoto(input: PostPhotoInput!): Photo!
+  }
+
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
 `
 let _id = 0
-const photos = []
-const resolvers = {
-    Query: {
-        totalPhotos: () => photos.length,
-        allPhotos: () => photos
-    },
-    Mutation: {
-        postPhoto(parent, args){
-            const newPhoto = {
-                id: _id++,
-                ...args.input
-            }
-            photos.push(newPhoto)
-            return newPhoto
-        }
-    },
-    Photo: {
-        url: parent => `http://yousite.com/img/${parent.id}.jpg`
-    }
 
+// サンプルデータ
+const users = [
+  {"githubLogin": "mHattrup", "name": "Mike Hattrup"},
+  {"githubLogin": "gPlake", "name": "Glen Plake"},
+  {"githubLogin": "sSchmidt", "name": "Scot Schmidt"},  
+]
+
+const photos = [
+  {
+    "id": "1",
+    "name": "Dropping the Heart Chute",
+    "description": "The heart chute is one of my favorite chutes",
+    "category": "ACTION",
+    "githubUser": "gPlake"
+  },
+  {
+    "id": "2",
+    "name": "Enjoying the sunshine",
+    "category": "SELFIE",
+    "githubUser": "sSchmidt"
+  },  
+  {
+    "id": "3",
+    "name": "Gunbarrel 25",
+    "description": "25 laps on gunbarrel today",
+    "category": "LANDSCAPE",
+    "githubUser": "sSchmidt"
+  },
+]
+
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+  Mutation: {
+      postPhoto(parent, args){
+          const newPhoto = {
+              id: _id++,
+              ...args.input
+          }
+          photos.push(newPhoto)
+          return newPhoto
+      }
+  },
+  Photo: {
+    url: parent => `http://yousite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin == parent.githubUser)
+    },
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    }
+  }
 }
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers
 })
 
 server.listen().then(({url}) => console.log(`GraphQL Service running on ${url}`))
